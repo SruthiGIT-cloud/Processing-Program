e2ea30826498ec441bfad927ff0baff50428e038
Sruthi A R <78865129+SruthiGIT-cloud@users.noreply.github.com>
2022-02-22 15:47:28 +0530

    Update fourballschallengeOOP.java

--- OOPS/fourballschallengeOOP.java
+++ OOPS/fourballschallengeOOP.java
@@ -2,48 +2,26 @@ import processing.core.PApplet;
 
 public class fourballschallenge extends PApplet{
 
-    private int WIDTH;
-    private int HEIGHT;
-    private int DIAMETER;
     private int x1=0,x2=0,x3=0,x4=0;
+    static fourballgetset obj;
 
-    public int getWIDTH() {
-        return WIDTH;
-    }
-
-    public void setWIDTH(int WIDTH) {
-        this.WIDTH = WIDTH;
-    }
-
-    public int getHEIGHT() {
-        return HEIGHT;
-    }
-
-    public void setHEIGHT(int HEIGHT) {
-        this.HEIGHT = HEIGHT;
-    }
-
-    public int getDIAMETER() {
-        return DIAMETER;
-    }
-
-    public void setDIAMETER(int DIAMETER) {
-        this.DIAMETER = DIAMETER;
-    }
 
     public static void main(String[] args) {
        PApplet.main("fourballschallenge", args);
-       fourballschallenge object1=new fourballschallenge();
-       object1.setWIDTH(680);
-        object1.setHEIGHT(480);
-       object1.setDIAMETER(20);
 
+       obj=new fourballgetset();
+       obj.setWidth(680);
+       obj.setHeight(480);
+       obj.setDiameter(20);
+       int a= obj.getDiameter();
+       int b=obj.getHeight();
+       int c= obj.getWidth();
     }
 
     @Override
     public void settings() {
         super.settings();
-        size(WIDTH, HEIGHT);
+        size(width,height);
     }
 
     @Override
@@ -53,10 +31,10 @@ public class fourballschallenge extends PApplet{
 
     @Override
     public void draw() {
-        ellipse(x1++,HEIGHT/5, DIAMETER, DIAMETER);
-        ellipse(x2+=2,HEIGHT*2/5, DIAMETER, DIAMETER);
-        ellipse(x3+=3,HEIGHT*3/5, DIAMETER, DIAMETER);
-        ellipse(x4+=4,HEIGHT*4/5, DIAMETER, DIAMETER);
+        ellipse(x1++,height/5,obj.getDiameter(),obj.getDiameter());
+        ellipse(x2+=2,height*2/5, obj.getDiameter(),obj.getDiameter());
+        ellipse(x3+=3,height*3/5, obj.getDiameter(),obj.getDiameter());
+        ellipse(x4+=4,height*4/5, obj.getDiameter(),obj.getDiameter());
     }
 
     private void paintWhite() {
